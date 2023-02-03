16a432106e9508b41363b7a3e166449b08509b1f
Cyrille <cypierre58@gmail.com>
2023-02-03 22:47:52 +0100

    Output sin at 440 Hz

--- lv2_mushroom/src/lv2_interface.c
+++ lv2_mushroom/src/lv2_interface.c
@@ -1,4 +1,5 @@
 #include <lv2.h>
+#include <math.h>
 #include <stdlib.h>
 
 char const URI[] = "ese-mushroom";
@@ -7,125 +8,78 @@ typedef enum {
 	PORT_OUTPUT = 0
 } PortIndex;
 
-/**
-   Every plugin defines a private structure for the plugin instance.  All data
-   associated with a plugin instance is stored here, and is available to
-   every instance method.  In this simple plugin, only port buffers need to be
-   stored, since there is no additional instance data.
-*/
 typedef struct {
 	float * output;
+	uint32_t time_offset;
+	float offset_to_angle;
 } PluginHandle;
 
-/**
-   The `instantiate()` function is called by the host to create a new plugin
-   instance.  The host passes the plugin descriptor, sample rate, and bundle
-   path for plugins that need to load additional resources (e.g. waveforms).
-   The features parameter contains host-provided features defined in LV2
-   extensions, but this simple plugin does not use any.
-
-   This function is in the ``instantiation'' threading class, so no other
-   methods on this instance will be called concurrently with it.
-*/
+/** Called by the host to create a new plugin instance. */
 static LV2_Handle instantiate(
 	const LV2_Descriptor *      descriptor,
 	double                      rate,
 	char const *                bundle_path,
 	LV2_Feature const * const * features)
 {
-  return (LV2_Handle)calloc(1, sizeof(PluginHandle));
+	PluginHandle * handle = calloc(1, sizeof(PluginHandle));
+	handle->offset_to_angle = 2 * M_PI / rate;
+	return handle;
 }
 
-/**
-   The `connect_port()` method is called by the host to connect a particular
-   port to a buffer.  The plugin must store the data location, but data may not
-   be accessed except in run().
-
-   This method is in the ``audio'' threading class, and is called in the same
-   context as run().
-*/
+/** Called by the host to connect a particular port to a buffer. */
 static void connect_port(LV2_Handle instance, uint32_t port, void* data) {
-  PluginHandle * handle = (PluginHandle *)instance;
+	PluginHandle * handle = instance;
 
-  switch((PortIndex)port) {
-  case PORT_OUTPUT:
-    handle->output = (float *)data;
-    break;
-  }
+	switch((PortIndex)port) {
+	case PORT_OUTPUT:
+		handle->output = (float *)data;
+		break;
+	}
 }
 
-/**
-   The `activate()` method is called by the host to initialise and prepare the
-   plugin instance for running.  The plugin must reset all internal state
-   except for buffer locations set by `connect_port()`.  Since this plugin has
-   no other internal state, this method does nothing.
-
-   This method is in the ``instantiation'' threading class, so no other
-   methods on this instance will be called concurrently with it.
-*/
-static void activate(LV2_Handle instance)
-{}
+/** Called by the host to initialise and prepare the plugin instance for running. */
+static void activate(LV2_Handle instance) {
+	PluginHandle * handle = instance;
+	handle->time_offset = 0;
+}
 
 /** Define a macro for converting a gain in dB to a coefficient. */
 #define DB_CO(g) ((g) > -90.0f ? powf(10.0f, (g)*0.05f) : 0.0f)
 
-/**
-   The `run()` method is the main process function of the plugin.  It processes
-   a block of audio in the audio context.  Since this plugin is
-   `lv2:hardRTCapable`, `run()` must be real-time safe, so blocking (e.g. with
-   a mutex) or memory allocation are not allowed.
-*/
-static void
-run(LV2_Handle instance, uint32_t n_samples)
-{
-  PluginHandle const * handle = (const PluginHandle*)instance;
+/** Main process function of the plugin. */
+static void run(LV2_Handle instance, uint32_t n_samples) {
+	PluginHandle * handle = instance;
+	float const freq = 440;
 
-  for(uint32_t pos = 0; pos < n_samples; pos++) {
-    handle->output[pos] = 0;
-  }
+	for(uint32_t pos = 0; pos < n_samples; pos++) {
+		handle->output[pos] = sinf(freq * handle->time_offset * handle->offset_to_angle);
+		++(handle->time_offset);
+	}
+}
+
+/** Called by the host after running the plugin. */
+static void deactivate(LV2_Handle instance) {
+}
+
+/** Destroy a plugin instance. */
+static void cleanup(LV2_Handle instance) {
+	free(instance);
 }
 
 /**
-   The `deactivate()` method is the counterpart to `activate()`, and is called
-   by the host after running the plugin.  It indicates that the host will not
-   call `run()` again until another call to `activate()` and is mainly useful
-   for more advanced plugins with ``live'' characteristics such as those with
-   auxiliary processing threads.  As with `activate()`, this plugin has no use
-   for this information so this method does nothing.
+  The `extension_data()` function returns any extension data supported by the
+  plugin.  Note that this is not an instance method, but a function on the
+  plugin descriptor.  It is usually used by plugins to implement additional
+  interfaces.  This plugin does not have any extension data, so this function
+  returns NULL.
 
-   This method is in the ``instantiation'' threading class, so no other
-   methods on this instance will be called concurrently with it.
-*/
-static void
-deactivate(LV2_Handle instance)
-{}
-
-/**
-   Destroy a plugin instance (counterpart to `instantiate()`).
-
-   This method is in the ``instantiation'' threading class, so no other
-   methods on this instance will be called concurrently with it.
-*/
-static void
-cleanup(LV2_Handle instance)
-{
-  free(instance);
-}
-
-/**
-   The `extension_data()` function returns any extension data supported by the
-   plugin.  Note that this is not an instance method, but a function on the
-   plugin descriptor.  It is usually used by plugins to implement additional
-   interfaces.  This plugin does not have any extension data, so this function
-   returns NULL.
-
-   This method is in the ``discovery'' threading class, so no other functions
-   or methods in this plugin library will be called concurrently with it.
-*/
-static const void*
+  This method is in the ``discovery'' threading class, so no other functions
+  or methods in this plugin library will be called concurrently with it.
+  */
+	static const void*
 extension_data(const char* uri)
 {
-  return NULL;
+	return NULL;
 }
 
 static const LV2_Descriptor descriptor = {
@@ -141,5 +95,5 @@ static const LV2_Descriptor descriptor = {
 
 LV2_SYMBOL_EXPORT
 const LV2_Descriptor * lv2_descriptor(uint32_t index) {
-  return index == 0 ? &descriptor : NULL;
+	return index == 0 ? &descriptor : NULL;
 }

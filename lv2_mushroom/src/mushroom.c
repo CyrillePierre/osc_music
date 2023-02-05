920912d233ee2ec4eba49433c8c890c12170057e
Cyrille <cypierre58@gmail.com>
2023-02-05 19:27:27 +0100

    Remove phase param

--- lv2_mushroom/src/mushroom.c
+++ lv2_mushroom/src/mushroom.c
@@ -4,29 +4,29 @@
 
 #include <math.h>
 
-static float sine(float pos, float phase)
+static float sine(float pos)
 {
-	return sinf(fmodf(pos + phase, 1) * 2 * M_PI);
+	return sinf(fmodf(pos, 1) * 2 * M_PI);
 }
 
-static float square(float pos, float phase)
+static float square(float pos)
 {
-	return fmodf(pos + phase, 1) < .5 ? 1 : -1;
+	return fmodf(pos, 1) < .5 ? 1 : -1;
 }
 
-static float sawtooth(float pos, float phase)
+static float sawtooth(float pos)
 {
-	return 2 * fmodf(pos + phase, 1) - 1;
+	return 2 * fmodf(pos, 1) - 1;
 }
 
-static float triangle(float pos, float phase)
+static float triangle(float pos)
 {
-	return 4 * fabs(fmodf(pos + phase - .25, 1) - .5) - 1;
+	return 4 * fabs(fmodf(pos - .25, 1) - .5) - 1;
 }
 
-static float enable(float pos, float phase, float width)
+static float enable(float pos, float width)
 {
-	return fmodf(pos + phase, 1) < width;
+	return fmodf(pos, 1) < width;
 }
 
 void run(LV2_Handle instance, uint32_t n_samples)
@@ -36,11 +36,10 @@ void run(LV2_Handle instance, uint32_t n_samples)
 	for(uint32_t i = 0; i < n_samples; i++) {
 		float pos   = 130 * handle->time_offset / handle->rate;
 		float pos2  = 10000 * handle->time_offset / handle->rate;
-		float shape = .6 * sine(pos, 0) * enable(pos, .25, .25)
-		              + .05 * enable(pos, 0, .75);
-		float left  = .2 * sine(pos2, 0) * shape;
-		float right = .05 * sine(pos2, .25) * shape + .3 * sawtooth(pos, 0);
-		left += .25 * (.5 * sawtooth(pos, 0) + .5) * sine(pos * .995 / 3, .25);
+		float shape = .6 * sine(pos) * enable(pos + .25, .25) + .05 * enable(pos, .75);
+		float left  = .2 * sine(pos2) * shape;
+		float right = .05 * sine(pos2 + .25) * shape + .3 * sawtooth(pos);
+		left += .25 * (.5 * sawtooth(pos) + .5) * sine(pos * .995 / 3 + .25);
 
 		handle->out_left[i]  = left;
 		handle->out_right[i] = right;

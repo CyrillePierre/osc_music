5bec4fa4b9bce362a1c526478f1b080caf2d815b
Cyrille <cypierre58@gmail.com>
2023-02-05 18:52:41 +0100

    Remove handle from waveform functions

--- lv2_mushroom/src/mushroom.c
+++ lv2_mushroom/src/mushroom.c
@@ -4,44 +4,44 @@
 
 #include <math.h>
 
-static float sine(PluginHandle* handle, float pos, float phase)
+static float sine(float pos, float phase)
 {
-	return sinf(fmodf(pos / handle->rate + phase, 1) * 2 * M_PI);
+	return sinf(fmodf(pos + phase, 1) * 2 * M_PI);
 }
 
-static float square(PluginHandle* handle, float pos, float phase)
+static float square(float pos, float phase)
 {
-	return fmodf(pos / handle->rate + phase, 1) < .5 ? 1 : -1;
+	return fmodf(pos + phase, 1) < .5 ? 1 : -1;
 }
 
-static float sawtooth(PluginHandle* handle, float pos, float phase)
+static float sawtooth(float pos, float phase)
 {
-	return 2 * fmodf(pos / handle->rate + phase, 1) - 1;
+	return 2 * fmodf(pos + phase, 1) - 1;
 }
 
-static float triangle(PluginHandle* handle, float pos, float phase)
+static float triangle(float pos, float phase)
 {
-	return 4 * fabs(fmodf(pos / handle->rate + phase - .25, 1) - .5) - 1;
+	return 4 * fabs(fmodf(pos + phase - .25, 1) - .5) - 1;
 }
 
-static float enable(PluginHandle* handle, float pos, float phase, float width)
+static float enable(float pos, float phase, float width)
 {
-	return fmodf(pos / handle->rate + phase, 1) < width;
+	return fmodf(pos + phase, 1) < width;
 }
 
 void run(LV2_Handle instance, uint32_t n_samples)
 {
 	PluginHandle* handle = instance;
-	float const   freq   = 100;
+	float const   freq   = 120;
 
 	for(uint32_t i = 0; i < n_samples; i++) {
-		float pos   = freq * handle->time_offset;
+		float pos   = freq * handle->time_offset / handle->rate;
 		float pos2  = 100.01 * pos;
-		float shape = .6 * sine(handle, pos, 0) * enable(handle, pos, .25, .25)
-		              + .05 * enable(handle, pos, 0, .75);
-		float left  = .2 * sine(handle, pos2, 0) * shape;
-		float right = .05 * sine(handle, pos2, .25) * shape + .3 * sawtooth(handle, pos, 0);
-		left += .1 * (.5 * sawtooth(handle, pos, 0) + .5) * sine(handle, pos * 1.007, .25);
+		float shape = .6 * sine(pos, 0) * enable(pos, .25, .25)
+		              + .05 * enable(pos, 0, .75);
+		float left  = .2 * sine(pos2, 0) * shape;
+		float right = .05 * sine(pos2, .25) * shape + .3 * sawtooth(pos, 0);
+		left += .25 * (.5 * sawtooth(pos, 0) + .5) * sine(pos * .99 / 3, .25);
 
 		handle->out_left[i]  = left;
 		handle->out_right[i] = right;
